8841c29485b488bbf9e1e6d0f2e56ed587eb8531
lorenuar s19 <lorenuar.s19@gmail.com>
2020-07-22 22:18:21 +0200

    Rnd chars generation done now, rnd flags + implementation

--- test.sh
+++ test.sh
@@ -12,11 +12,62 @@
 #                                                                              #
 # **************************************************************************** #
 
-# Vars
+# =============================== Global variables ============================#
 header_file=.test.header.h
 printf_main_file=.test.printf.c
 ft_printf_main_file=.test.ft_printf.c
 
+
+# RND_CHARS
+MAX_RND_CHARS=1000
+
+# SEQUENCE
+MAX_ITEMS=42
+# RND_FLAGS
+MAX_NUM=50
+NUM_CONV="iduxX"
+STR_CONV='s'
+CHR_CONV='c'
+PTR_CONV='p'
+
+# ================================= Functions =================================#
+function write_main_printf()
+{
+    rm -f $printf_main_file
+    echo "/*" >> $printf_main_file
+    echo "** Main for testing the stdio printf" >> $printf_main_file
+    echo "**" >> $printf_main_file
+    echo "** by : lorenuar" >> $printf_main_file
+    echo "*/" >> $printf_main_file
+    echo "" >> $printf_main_file
+    echo "#include \"$header_file"\" >> $printf_main_file
+    echo "" >> $printf_main_file
+    echo "int"$'\t'"main(void)" >> $printf_main_file
+    echo "{" >> $printf_main_file
+    echo $'\t'"printf(TEST);" >> $printf_main_file
+    echo $'\t'"return (0);" >> $printf_main_file
+    echo "}" >> $printf_main_file
+    echo "" >> $printf_main_file
+}
+
+function write_main_ft_printf()
+{
+    rm -f $ft_printf_main_file
+    echo "/*" >> $ft_printf_main_file
+    echo "** Main for testing your ft_printf" >> $ft_printf_main_file
+    echo "**" >> $ft_printf_main_file
+    echo "** by : lorenuar" >> $ft_printf_main_file
+    echo "*/" >> $ft_printf_main_file
+    echo "" >> $ft_printf_main_file
+    echo "#include \"$header_file"\" >> $ft_printf_main_file
+    echo "" >> $ft_printf_main_file
+    echo "int"$'\t'"main(void)" >> $ft_printf_main_file
+    echo "{" >> $ft_printf_main_file
+    echo $'\t'"ft_printf(TEST);" >> $ft_printf_main_file
+    echo $'\t'"return (0);" >> $ft_printf_main_file
+    echo "}" >> $ft_printf_main_file
+    echo "" >> $ft_printf_main_file
+}
 
 function write_header_file()
 {
@@ -30,7 +81,7 @@ function write_header_file()
 
     echo "" >> $header_file
     echo "#ifndef TEST_HEADER_H" >> $header_file
-    echo "# include \"ft_printf.h\"" >> $header_file
+    echo "//# include \"ft_printf.h\"" >> $header_file
     echo "# include <stdio.h>" >> $header_file
     echo "" >> $header_file
     
@@ -51,46 +102,29 @@ function str_contains ()
     return 0
 }
 
-function write_rnd_chars()
+function rnd_chars()
 {
     local n_rand_chars=$((RANDOM%$1))
+    local chars="!#$%&'*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ^_abcdefghijklmnopqrstuvwxyz"
+    local print=42
 
-    local excluded_chars=$'\n\\\'\"/'
-
-    local char_min=$((20))
-    local char_max=$((127-char_min-2))
     for (( i=0; i<= n_rand_chars; i++))
     do
-        tmp=$(printf "$(printf \\%1o $((char_min + $RANDOM % char_max)) )" )
-        dummy=$RANDOM
-        
-
-    local print=50
-        if ! str_contains  $excluded_chars $tmp
+        tmp=${chars:$((RANDOM%${#chars})):1}
+      
+        if [[ $((i%print)) -eq 0 ]]
         then
-
-            if [[ $((i%print)) -eq 1 ]]
-            then
-                #printf "\033[31m-\033[m\n"
-            fi
-            i=$((i-1))
-        else
-            if [[ $((i%print)) -eq 1 ]]
-            then
-                printf "\033[32m+\033[m"
-            fi
-            macro+=$tmp
+            printf +
         fi
-
+        macro+=$tmp
     done
-
 }
 
 function write_header_file_macro()
 {
     macro="# define TEST \""
-   
-    write_rnd_chars 50
+
+    rnd_chars $MAX_RND_CHARS
 
     macro+="\""
 
@@ -98,6 +132,57 @@ function write_header_file_macro()
     echo $'\n'$macro
 }
 
+function gen_flag ()
+{
+    local tmp_flag='%'
+    local is_percent=$(($RANDOM%5))
+    if [[ $is_percent -eq 1 ]]
+    then
+        tmp_flag+='%'
+        flag=$tmp_flag
+        return 0
+    fi
+
+    local conv=''
+    case $1 in
+        rnd) return 0 ;;
+        num) conv=${NUM_CONV:${$(($RANDOM%${#NUM_CONV}))}:1} ;;
+        str) conv=$STR_CONV ;;
+        chr) conv=$CHR_CONV ;;
+        ptr) conv=$PTR_CONV ;;
+        *) return 1 ;;
+    esac
+
+    local has_prs=$(($RANDOM%2))
+    
+    tmp_flag+=$(($RANDOM%$MAX_NUM))
+    if [[ $has_prs -eq 1 ]]
+    then
+        tmp_flag+='.'
+        tmp_flag+=$(($RANDOM%$MAX_NUM))
+    fi
+    tmp_flag+=$conv
+    flag=$tmp_flag
+}
+
+function gen_sequence ()
+{
+    local max_items=$(($RANDOM%$MAX_ITEMS))
+    items=( rnd num str chr ptr)
+    sequence=()
+    for (( it=0; it<max_items; it++ ))
+    do
+        sequence+=(${items[$(($RANDOM % $((${#items}+1))))]})
+    done
+}
+
+# ================================== Program ================================= #
+
+write_main_ft_printf
+write_main_printf
 write_header_file
 
-rm -f $header_file
+gen_sequence
+
+sleep .1
+rm -f $header_file $printf_main_file $ft_printf_main_file
