e9e2c30ef4216f3300e67fa258b8aa4139ada90b
lorenuar s19 <lorenuar.s19@gmail.com>
2020-08-20 13:50:07 +0200

    E  X  P  A  N  D

--- test.sh
+++ test.sh
@@ -15,7 +15,7 @@
 # =============================== Global variables =========================== #
 # = = = = = = = = = = = = = = = YOU CAN TWEAK THESES = = = = = = = = = = = = = #
 CCFLAGS="-Wall -Wextra -Werror"
-# Specify the verbose level : 0 = pretty , 1 = minimal, 2 = full info 
+# Specify the verbose level : 0 = pretty , 1 = minimal, 2 = full info
 _VERBOSE=2
 # The time out for execution of tests
 _TIME_OUT=1
@@ -65,22 +65,22 @@ function check_up_to_date ()
     curl -s $1 -o $nfile
     diff -u $ofile $nfile >/dev/null
     local diff_ret=$?
-    if [[ $diff_ret -eq 1 ]]; then
-        while true; do
-        read -p "New version exists do you want to update ?[Y/n]" yn
+    if [[ $diff_ret -eq 1 ]]
+    then
+        while true
+        do
+            read -p "New version exists do you want to update ?[Y/n]" yn
             case $yn in
-                [Nn]* ) 
+                [Nn]* )
                     printf "\033[033mYou cancelled the update\033[0m\n"
                     rm -rf $checkdir $ofile $nfile
-                    return
-                ;;
-                [Yy]* | * )  
+                return;;
+                [Yy]* | * )
                     printf "\033[33mDownloading new update ..."
                     cp $nfile $0
                     printf " \033[32;1mUpdated successfully.\033[0m\n"
                     rm -rf $checkdir $ofile $nfile
-                    return
-                ;;
+                return;;
             esac
         done
     else
@@ -89,8 +89,8 @@ function check_up_to_date ()
     rm -rf $checkdir $ofile $nfile
 }
 
-function write_main_files() 
-{   
+function write_main_files()
+{
     # Writes the main for STDIO Printf
     rm -f $printf_main_file
     echo "\
@@ -107,8 +107,8 @@ int		main(void)
 	printf(TEST);
 	return (0);
 }
-" >> $printf_main_file
-    # Writes the main for your ft_printf 
+    " >> $printf_main_file
+    # Writes the main for your ft_printf
     rm -f $ft_printf_main_file
     echo "\
 /*
@@ -124,7 +124,7 @@ int		main(void)
 	ft_printf(TEST);
 	return (0);
 }
-" >> $ft_printf_main_file
+    " >> $ft_printf_main_file
 }
 
 function write_header_file()
@@ -142,7 +142,7 @@ function write_header_file()
 #ifndef HEADER_H
 # include \"$FT_PRINTF_HEADER_FILE\"
 # include <stdio.h>
-" >> $header_file
+    " >> $header_file
     # This is where all the magic happens
     write_sequence
 }
@@ -162,21 +162,51 @@ function gen_flag ()
 {
     flag=""
     case $1 in
-        FLG) flag="%"; return 0;;
-        NO) flag=""; return 0;;
-        SP) flag=" "; return 0;;
-        ZR) flag="0"; return 0;;
-        ST*) set -f; flag="*"; return 0;;
-        DT) flag="."; return 0;;
-        FW) rnd_number $FLAG_NUM_MAX; flag=$R_NUM; return 0;;
-        PR) rnd_number $FLAG_NUM_MAX 0; flag=$R_NUM; return 0;;
-        CV_CHR) flag='c'; return 0;;
-        CV_STR) flag='s'; return 0;;
-        CV_PTR) flag='p'; return 0;;
-        CV_NUM) flag=${NUM_CONV:$(($RANDOM%${#NUM_CONV})):1}; return 0;;
-
-        rnd) flag=""; rnd_chars $MAX_RND_CHARS; return 0;;
-        *) return 1;;
+        FLG)
+            flag="%"
+        return 0;;
+        NO)
+            flag=""
+        return 0;;
+        SP)
+            flag=" "
+        return 0;;
+        ZR)
+            flag="0"
+        return 0;;
+        ST*)
+            set -f
+            flag="*"
+        return 0;;
+        DT)
+            flag="."
+        return 0;;
+        FW)
+            rnd_number $FLAG_NUM_MAX
+            flag=$R_NUM
+        return 0;;
+        PR)
+            rnd_number $FLAG_NUM_MAX 0
+            flag=$R_NUM
+        return 0;;
+        CV_CHR)
+            flag='c'
+        return 0;;
+        CV_STR)
+            flag='s'
+        return 0;;
+        CV_PTR)
+            flag='p'
+        return 0;;
+        CV_NUM)
+            flag=${NUM_CONV:$(($RANDOM%${#NUM_CONV})):1}
+        return 0;;
+        rnd)
+            flag=""
+            rnd_chars $MAX_RND_CHARS
+        return 0;;
+        *)
+        return 1;;
     esac
 }
 
@@ -185,13 +215,32 @@ function gen_var ()
     flag=''
     local is_null=$(($RANDOM%10))
     case $1 in
-        ST_FW) rnd_number $FLAG_NUM_MAX; flag=$R_NUM; return 0;;
-        ST_PR) rnd_number $FLAG_NUM_MAX 0; flag=$R_NUM; return 0;;
-        CV_CHR) rnd_number 127 0; flag=$R_NUM; return 0;;
-        CV_STR) flag+=\"; rnd_chars $MAX_RND_CHARS; flag+=\"; return 0;;
-        CV_PTR) flag="NULL"; return 0;;
-        CV_NUM) rnd_number $FLAG_NUM_MAX; flag=$R_NUM; return 0;;
-        *) return 0;;
+        ST_FW)
+            rnd_number $FLAG_NUM_MAX
+            flag=$R_NUM
+        return 0;;
+        ST_PR)
+            rnd_number $FLAG_NUM_MAX 0
+            flag=$R_NUM
+        return 0;;
+        CV_CHR)
+            rnd_number 127 0
+            flag=$R_NUM
+        return 0;;
+        CV_STR)
+            flag+=\"
+            rnd_chars $MAX_RND_CHARS
+            flag+=\"
+        return 0;;
+        CV_PTR)
+            flag="NULL"
+        return 0;;
+        CV_NUM)
+            rnd_number $FLAG_NUM_MAX
+            flag=$R_NUM
+        return 0;;
+        *)
+        return 0;;
     esac
 }
 
@@ -199,7 +248,8 @@ function rnd_number ()
 {
     local negative=$(($RANDOM%2))
     R_NUM=$(($RANDOM%$1))
-    if [[ negative -eq 1 ]] && [[ -z $2 ]]; then
+    if [[ negative -eq 1 ]] && [[ -z $2 ]]
+    then
         R_NUM=$((-$R_NUM))
     fi
 }
@@ -222,28 +272,34 @@ function gen_sequence ()
             local fw_star=$(($RANDOM%2))
             local pr_star=$(($RANDOM%2))
             local conv=${conv_items[$(($RANDOM%${#conv_items[@]}))]}
-
-            if [[ $has_precision -eq 1 ]]; then
+            
+            if [[ $has_precision -eq 1 ]]
+            then
                 zero_items=( SP NO )
             fi
-            if [[ $conv = "CV_STR" ]] || [[ $conv = "CV_CHR" ]] || [[ $conv = "CV_PTR" ]]; then
+            if [[ $conv = "CV_STR" ]] || [[ $conv = "CV_CHR" ]] || [[ $conv = "CV_PTR" ]]
+            then
                 zero_items=( NO )
             fi
-            if [[ $conv = "CV_PTR" ]] || [[ $conv = "CV_CHR" ]]; then
+            if [[ $conv = "CV_PTR" ]] || [[ $conv = "CV_CHR" ]]
+            then
                 has_precision=0
             fi
-
+            
             local zero_space_nothing=${zero_items[$(($RANDOM%${#zero_items[@]}))]}
             sequence+=( $zero_space_nothing )
             
-            if [[ $fw_star -eq 1 ]]; then
+            if [[ $fw_star -eq 1 ]]
+            then
                 sequence+=( ST_FW )
             else
                 sequence+=( FW )
             fi
-            if [[ $has_precision -eq 1 ]]; then
+            if [[ $has_precision -eq 1 ]]
+            then
                 sequence+=( DT )
-                if [[ $pr_star -eq 1 ]]; then
+                if [[ $pr_star -eq 1 ]]
+                then
                     sequence+=( ST_PR )
                 else
                     sequence+=( PR )
@@ -295,7 +351,7 @@ time_out_clean_up()
     trap - ALRM
     kill -ALRM $a 2>/dev/null
     kill $! 2>/dev/null &&
-      return 124
+    return 124
 }
 
 time_out_watcher()
@@ -310,7 +366,8 @@ time_out ()
     time_out_watcher $1& a=$!
     shift
     trap "time_out_clean_up" ALRM 2>/dev/null
-    "$@" 2>/dev/null & wait $! 2>/dev/null; RET=$?
+    "$@" 2>/dev/null & wait $! 2>/dev/null
+    RET=$?
     kill -ALRM $a 2>/dev/null
     wait $a 2>/dev/null
     return $RET
@@ -319,8 +376,10 @@ time_out ()
 function compile_run()
 {
     rm -f $printf_exec_file
-    if [[ -e $printf_main_file ]]; then
-        if ! gcc $CCFLAGS -I. $printf_main_file -o $printf_exec_file 2>/dev/null; then 
+    if [[ -e $printf_main_file ]]
+    then
+        if ! gcc $CCFLAGS -I. $printf_main_file -o $printf_exec_file 2>/dev/null
+        then
             if [[ $_VERBOSE -ge 3 ]]
             then
                 printf "\nTEST : %-6d \033[31;1m STDIO PRINTF COMPILATION ERROR (tester made invalid pattern)\033[m\n" $test_n
@@ -330,7 +389,6 @@ function compile_run()
             macro=''
             sequence=()
             run_test $test_n
-            sleep 2
             return 0
         fi
     fi
@@ -340,14 +398,17 @@ function compile_run()
     else
         STD_RET=$?
         printf "\n= = = TEST : %-6d STDIO PRINTF TIMEOUT or EXEC ERROR %03d\n" $test_n $STD_RET >> $LOG_FILE"_"$test_n
-		echo $macro >> $LOG_FILE"_"$test_n
+        echo $macro >> $LOG_FILE"_"$test_n
         return 1
     fi
     
     rm -f $ft_printf_exec_file
-    if [[ -e $ft_printf_main_file ]]; then
-       if ! gcc $CCFLAGS -I../ $ft_printf_main_file $FT_PRINTF_LIB_FILE -o $ft_printf_exec_file; then 
-        printf "\033[1;31mCOMPILE ERROR\033[m\n"; return 3
+    if [[ -e $ft_printf_main_file ]]
+    then
+        if ! gcc $CCFLAGS -I../ $ft_printf_main_file $FT_PRINTF_LIB_FILE -o $ft_printf_exec_file
+        then
+            printf "\033[1;31mCOMPILE ERROR\033[m\n"
+            return 3
         fi
     fi
     if [[ -e $ft_printf_exec_file ]] && time_out $_TIME_OUT ./$ft_printf_exec_file > $ft_printf_diff_file
@@ -358,10 +419,10 @@ function compile_run()
         then
             printf "\nTEST : %-6d \033[31;1m FT_printf TIME OUT or EXEC ERROR\033[m\n" $test_n
             echo $macro
-            KO_NUM=$((KO_NUM + 1))
+            TO_NUM=$((TO_NUM + 1))
         fi
         printf "\n= = = TEST : %-6d FT_printf TIME OUT or EXEC ERROR\n" $test_n >> $LOG_FILE"_"$test_n
-		echo $macro >> $LOG_FILE"_"$test_n
+        echo $macro >> $LOG_FILE"_"$test_n
         return 2
     fi
 }
@@ -369,75 +430,83 @@ function compile_run()
 function run_test ()
 {
     write_header_file
-    
-    if ! compile_run; then return 1
-    fi   
-
+    if ! compile_run
+    then
+        return 1
+    fi
     printf "\n= = = TEST : %-6d = =\n" $test_n >> $LOG_FILE"_"$test_n
     echo $macro >> $LOG_FILE"_"$test_n
-	
-    if [[ $_VERBOSE -ge 1 ]]; then printf "TEST% 6d : " $test_n
-	fi
-    
-    if [[ -e $ft_printf_diff_file ]] && [[ -e $printf_diff_file ]]; then
+    if [[ $_VERBOSE -ge 1 ]]
+    then
+        printf "TEST% 6d : " $test_n
+    fi
+    if [[ -e $ft_printf_diff_file ]] && [[ -e $printf_diff_file ]]
+    then
         diff -a -s -u --label FT_42 $ft_printf_diff_file --label STDIO $printf_diff_file >> $LOG_FILE"_"$test_n
     fi
-    
     if [[ $? -eq 0 ]]
     then
-		if [[ $_VERBOSE -ge 1 ]]
-		then
-        	printf "\033[32;1m + OK +\033[m\n" $test_n
-			cat $ft_printf_diff_file >> $LOG_FILE"_"$test_n
-			if [[ $_VERBOSE -ge 2 ]]
-			then
-				echo $macro
-				cat $ft_printf_diff_file
-			fi
-		elif [[ $_VERBOSE -eq 0 ]]; then printf "\033[32;1m*\033[m"
-		fi
+        if [[ $_VERBOSE -ge 1 ]]
+        then
+            printf "\033[32;1m + OK +\033[m\n" $test_n
+            cat $ft_printf_diff_file >> $LOG_FILE"_"$test_n
+            if [[ $_VERBOSE -ge 2 ]]
+            then
+                echo $macro
+                cat $ft_printf_diff_file
+            fi
+        elif [[ $_VERBOSE -eq 0 ]]
+        then
+            printf "\033[32;1m*\033[m"
+        fi
         echo OK >> $LOG_FILE"_"$test_n
-		OK_NUM=$((OK_NUM + 1))
-		return 0
+        OK_NUM=$((OK_NUM + 1))
+        return 0
     else
-		if [[ $_VERBOSE -ge 1 ]]
-		then
-        	printf "\033[31;1m ! KO !\033[m\n" $test_n
-			if [[ $_VERBOSE -ge 2 ]]
-			then
-    			echo $macro
-                if [[ -e $ft_printf_diff_file ]] && [[ -e $printf_diff_file ]]; then
-                    if [[ $(uname) == "Linux" ]]; then
-        		        diff --color=always -a -u --label FT_42 $ft_printf_diff_file --label STDIO $printf_diff_file
+        if [[ $_VERBOSE -ge 1 ]]
+        then
+            printf "\033[31;1m ! KO !\033[m\n" $test_n
+            if [[ $_VERBOSE -ge 2 ]]
+            then
+                echo $macro
+                if [[ -e $ft_printf_diff_file ]] && [[ -e $printf_diff_file ]]
+                then
+                    if [[ $(uname) == "Linux" ]]
+                    then
+                        diff --color=always -a -u --label FT_42 $ft_printf_diff_file --label STDIO $printf_diff_file
                     else
-        		        diff -a -u --label FT_42 $ft_printf_diff_file --label STDIO $printf_diff_file
+                        diff -a -u --label FT_42 $ft_printf_diff_file --label STDIO $printf_diff_file
                     fi
                 fi
-			fi
-		elif [[ $_VERBOSE -eq 0 ]]; then printf "\033[31;1m!%-6d\033[m" $test_n
-		fi
-		KO_NUM=$((KO_NUM + 1))
-		return 2
+            fi
+        elif [[ $_VERBOSE -eq 0 ]]
+        then
+            printf "\033[31;1m!%-6d\033[m" $test_n
+        fi
+        KO_NUM=$((KO_NUM + 1))
+        return 2
     fi
 }
 
 function print_summary ()
 {
-    if [[ $test_n -le 0 ]]; then 
+    if [[ $test_n -le 0 ]]
+    then
         test_n=$(($test_n + 1))
     fi
-	local percent_ko=$(perl -e 'print '${KO_NUM}'.0 / '${test_n}'.0 * 100' )
-	printf "\nSummary : \033[32;1m%6d OK \033[31;1m%6d KO \033[37;1m%6d TOTAL\033[m\t" $OK_NUM $KO_NUM $test_n
-	if [[ ${percent_ko//.*} -lt 40 ]] && [[ ${percent_ko//*} != '0' ]]; then 
-		printf "\033[32;1mPercent KO : %f%%\033[m\n" $percent_ko
-	else
-		printf "\033[31;1mPercent KO : %f%%\033[m\n" $percent_ko
-	fi
+    local percent_ko=$(perl -e 'print '${KO_NUM}'.0 / '${test_n}'.0 * 100' )
+    printf "\nSummary : \033[32;1m%6d OK \033[31;1m%6d KO \033[33;1m%6d TO\033[37;1m%6d TOTAL\033[m\t" $OK_NUM $KO_NUM $TO_NUM $test_n
+    if [[ ${percent_ko//.*} -lt 40 ]] && [[ ${percent_ko//*} != '0' ]]
+    then
+        printf "\033[32;1mPercent KO : %f%%\033[m\n" $percent_ko
+    else
+        printf "\033[31;1mPercent KO : %f%%\033[m\n" $percent_ko
+    fi
 }
 
 function cleanup ()
 {
-	print_summary
+    print_summary
     rm -f $printf_diff_file $printf_exec_file $printf_main_file $ft_printf_diff_file $ft_printf_exec_file $ft_printf_main_file
     rm -rf $checkdir $ofile $nfile
     exit
@@ -445,11 +514,22 @@ function cleanup ()
 
 function input_files()
 {
-    if [[ -e Makefile ]] || [[ -e makefile ]]; then make 2>/dev/null; fi
+    if [[ -e Makefile ]] || [[ -e makefile ]]
+    then
+        make
+    fi
     FT_PRINTF_HEADER_FILE=$( cd $WD && find ../ -type f -name "ft_printf.h" )
-    if [[ -z $FT_PRINTF_HEADER_FILE ]]; then echo "ft_printf.h Not Found"; exit 1; fi
+    if [[ -z $FT_PRINTF_HEADER_FILE ]]
+    then
+        echo "ft_printf.h Not Found"
+        exit 1
+    fi
     FT_PRINTF_LIB_FILE=$( find . -name "libftprintf.a" )
-    if [[ -z $FT_PRINTF_LIB_FILE ]]; then echo "libftprintf.a Not Found"; exit 1; fi
+    if [[ -z $FT_PRINTF_LIB_FILE ]]
+    then
+        echo "libftprintf.a Not Found"
+        exit 1
+    fi
 }
 
 function usage  ()
@@ -477,28 +557,46 @@ MAX_TESTS=5
 _VERBOSE=0
 _GLOBAL_MAX_=4
 
-if [[ $# -eq 0 ]]; then usage; fi
-while [[ $1 != "" ]]; do
+if [[ $# -eq 0 ]]
+then
+    usage
+fi
+while [[ $1 != "" ]]
+do
     case $1 in
-    -v | --verbose )    shift; _VERBOSE=$1;;
-    -t | --tests )      shift; MAX_TESTS=$1;;
-    -c | --comp )       shift; _GLOBAL_MAX_=$1;;
-    -d | --no-ko )      NO_DISPLAY=1;;
-    -u | --update )     check_up_to_date "https://raw.githubusercontent.com/lorenuars19/ft_printf_tester/master/test.sh" ;;
-    -h | --help | *) usage ;;
+        -v | --verbose )
+            shift
+        _VERBOSE=$1;;
+        -t | --tests )
+            shift
+        MAX_TESTS=$1;;
+        -c | --comp )
+            shift
+        _GLOBAL_MAX_=$1;;
+        -d | --no-ko )
+        NO_DISPLAY=1;;
+        -u | --update )
+        check_up_to_date "https://raw.githubusercontent.com/lorenuars19/ft_printf_tester/master/test.sh";;
+        -h | --help | *)
+        usage;;
     esac
     shift
 done
 
-if [[ $_GLOBAL_MAX_ -le 0 ]]; then _GLOBAL_MAX_=1; fi
+if [[ $_GLOBAL_MAX_ -le 0 ]]
+then
+    _GLOBAL_MAX_=1
+fi
 # Max number of generated chars
 MAX_RND_CHARS=$(( 1 + ($_GLOBAL_MAX_/10 )))
 # Max number of sequence items
 MAX_SEQ_ITEMS=$(( $_GLOBAL_MAX_/2 ))
 # Max number for flag params
 FLAG_NUM_MAX=20
-if [[ $FLAG_NUM_MAX -ge 42 ]]; then FLAG_NUM_MAX=42; fi
-
+if [[ $FLAG_NUM_MAX -ge 42 ]]
+then
+    FLAG_NUM_MAX=42
+fi
 # Delete old LOGS
 rm -f $LOG_FILE*
 # Create Working Directory
@@ -512,14 +610,15 @@ trap cleanup SIGINT EXIT SIGSEGV SIGABRT SIGBUS
 # Run the tests
 OK_NUM=0
 KO_NUM=0
-declare -a KO_ARR
+TO_NUM=0
+declare -a KO_ARR TO_ARR
 for (( test_n=0;test_n<$MAX_TESTS; test_n++ ))
 do
     run_test $test_n
-	status=$?
-	if [[ $status -eq 2 ]] 
-	then
-		KO_ARR+=( $LOG_FILE"_"$test_n )
+    status=$?
+    if [[ $status -eq 2 ]]
+    then
+        KO_ARR+=( $LOG_FILE"_"$test_n )
     fi
 done
 if [[ KO_NUM -gt 0 ]]
@@ -529,13 +628,18 @@ then
     && echo "cat "${KO_ARR[@]}" | less" > $LOG_DISPLAY_FILE \
     && chmod 775 $LOG_DISPLAY_FILE
     printf "\033[36;1m\n./%s\n\033[mTo display KO Logs at anytime\n" $LOG_DISPLAY_FILE
-
-    while true && [[ $NO_DISPLAY -eq 0 ]]; do
+    
+    while true && [[ $NO_DISPLAY -eq 0 ]]
+    do
         read -p "Do you want to display all the logs of the failed tests in less ?[y/N]" yn
         case $yn in
-            [Yy]* ) ./$LOG_DISPLAY_FILE; break;;
-            [Nn]* ) exit;;
-            * ) exit;;
+            [Yy]* )
+                ./$LOG_DISPLAY_FILE
+            break;;
+            [Nn]* )
+            exit;;
+            * )
+            exit;;
         esac
     done
 fi
